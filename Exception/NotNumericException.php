52169df605e7e705b84410ea5d47605a76d38b62
Tobias Franek <tobias.franek@gmail.com>
2018-06-16 15:18:30 +0200

    added NotNumericException

---
+++ Exception/NotNumericException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tfranek\APIUtilBundle\Exception;
+
+/**
+ * NotNumericException Exception
+ * @author Tobias Franek <tobias.franek@gmail.com>
+ * @license MIT
+ */
+class NotNumericException extends \DomainException {}
