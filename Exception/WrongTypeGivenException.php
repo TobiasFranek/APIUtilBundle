e465deab671017a82b7aaa63e185ded35be46471
Tobias Franek <tobias.franek@gmail.com>
2018-06-16 14:48:52 +0200

    added an Exception

---
+++ Exception/WrongTypeGivenException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Tfranek\APIUtilBundle\Exception;
+
+/**
+ * WrongTypeGivenException Exception
+ * @author Tobias Franek <tobias.franek@gmail.com>
+ * @license MIT
+ */
+class WrongTypeGivenException extends \DomainException {}
